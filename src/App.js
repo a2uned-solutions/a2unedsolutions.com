feef17e3a8d96e2e60ecc94a432602d41201ef22
nathanolmstead <nathan@a2unedsolutions.com>
2018-01-01 19:50:21 -0500

    more global styles, connection footer links, deploy prod script

--- src/App.js
+++ src/App.js
@@ -11,21 +11,45 @@ class App extends Component {
         </header>
         <section id="hero">
             <h1>
-                <span className="small">We design &amp; develop</span>
-                <span className="dark">Web</span> Applications
-                <span className="dark">
+                <span className="small-title-caps">We design &amp; develop</span>
+                <span className="c-secondary">Web</span> Applications
+                <span className="c-secondary">
                     <span className="thin"> &amp; </span>
                     Blockchain
                 </span>
-                <span className="small">Technology</span>
+                <span className="small-title-caps">Technology</span>
             </h1>
         </section>
         <section id="intro">
             <aside className="content">
                 <h2>Growing with Decentralized Applications</h2>
-                <p>With the rise of Bitcoin and the Ethereum Network, we believe the disruptive power of blockchain technology has just begun. Our goal is to create engaging and accessible user interfaces that seamlessly connect with back-end API’s. Many of our projects will be built on decentralized networks, utilize blockchain technology, and work with crytpocurrencies.</p>
+                <p>With the rise of Bitcoin and the Ethereum Network, we believe the disruptive power of blockchain technology has just begun. <em>Our goal</em> is to create engaging and accessible user interfaces that seamlessly connect with back-end API’s. Many of our projects will be built on decentralized networks, utilize blockchain technology, and work with crytpocurrencies.</p>
             </aside>
         </section>
+        <footer>
+            <ul className="connect no-list">
+                <li>
+                    <a href="https://t.me/a2uned" target="_blank" rel="noopener noreferrer" title="Telegram">
+                        <i className="fa fa-telegram" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/a2uned" target="_blank" rel="noopener noreferrer" title="Twitter">
+                        <i className="fa fa-twitter-square" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/a2uned-solutions" target="_blank" rel="noopener noreferrer" title="GitHub">
+                        <i className="fa fa-github" aria-hidden="true"></i>
+                    </a>
+                </li>
+                {/*<li>*/}
+                    {/*<a href="" target="_blank">*/}
+                        {/*<i className="fa fa-linkedin-square" target="_blank" rel="noopener noreferrer"  aria-hidden="true"></i>*/}
+                    {/*</a>*/}
+                {/*</li>*/}
+            </ul>
+        </footer>
       </div>
     );
   }
