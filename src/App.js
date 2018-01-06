e3ce36d43390bd4d0f73f84d57fea5da1afe401f
nathanolmstead <nathan@a2unedsolutions.com>
2018-01-05 20:21:59 -0500

    Images and desktop styling

--- src/App.js
+++ src/App.js
@@ -12,12 +12,15 @@ class App extends Component {
         <section id="hero">
             <h1>
                 <span className="small-title-caps">We design &amp; develop</span>
-                <span className="c-secondary">Web</span> Applications
-                <span className="c-secondary">
-                    <span className="thin"> &amp; </span>
-                    Blockchain
+                <span className="c-secondary-text">Web</span> Applications
+                <span className="block">
+                    Dapps
+                    <span className="c-secondary-text">
+                        <span className="thin"> &amp; </span>
+                        Blockchain
+                    </span>
                 </span>
-                <span className="small-title-caps">Technology</span>
+                <span className="small-title-caps align-right">Technology</span>
             </h1>
         </section>
         <section id="intro">
@@ -25,6 +28,16 @@ class App extends Component {
                 <h2>Growing with Decentralized Applications</h2>
                 <p>With the rise of Bitcoin and the Ethereum Network, we believe the disruptive power of blockchain technology has just begun. <em>Our goal</em> is to create engaging and accessible user interfaces that seamlessly connect with back-end API’s. Many of our projects will be built on decentralized networks, utilize blockchain technology, and work with crytpocurrencies.</p>
             </aside>
+        </section>
+        <section id="techstack">
+            <h2>User Experience <span className="thin">+</span> Full Stack Development</h2>
+        </section>
+        <section className="content-bottom">
+            <ul className="no-list">
+                <li>&lt;- Contact</li>
+                <li>&lt;- Connect</li>
+                <li>&nbsp;</li>
+            </ul>
         </section>
         <footer>
             <ul className="connect no-list">
