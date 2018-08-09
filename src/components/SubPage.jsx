1d083b6c873f4aac524d03061908356baec02601
nathanolmstead <nathan@a2unedsolutions.com>
2018-08-09 09:42:00 -0400

    Update sub page heading animation

--- src/components/SubPage.jsx
+++ src/components/SubPage.jsx
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import {
-  Link,
-} from 'react-router-dom';
 
 import Clients from './Clients';
 import Services from './Services';
@@ -10,10 +6,7 @@ import AboutUs from './AboutUs';
 
 class SubPage extends Component {
 
-  timer = null;
-
   state = {
-    completed: 0,
     loaded: false,
     title: '',
     classNames: '',
@@ -24,28 +17,15 @@ class SubPage extends Component {
 
     this.changePageData()
 
-    this.timer = setInterval(this.progress, 40);
-
     setTimeout(
       function() {
         this.setState({ loaded: true });
-      }.bind(this), 1100
+      }.bind(this), 400
     );
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer);
     this.setState({ loaded: false });
-  }
-
-  progress = () => {
-    const { completed } = this.state;
-    if (completed > 100) {
-      return;
-    } else {
-      const diff = Math.random() * 20;
-      this.setState({ completed: Math.min(completed + diff, 100) });
-    }
   }
 
   changePageData() {
@@ -78,17 +58,13 @@ class SubPage extends Component {
 
   render() {
 
-    const { completed, loaded, title, classNames, component } = this.state;
+    const { loaded, title, classNames, component } = this.state;
 
     return (
       <section className={`${classNames} sub-page ${loaded && 'loaded'}`}>
         <header className="page-header">
-          <Link to="/" title="Home">
-            <i className="fas fa-home"></i>
-          </Link>
-          <span className="delim"> / </span>
           <h1>{title}</h1>
-          <LinearProgress className="page-progress" variant="determinate" value={completed} />
+          <span className="border"></span>
         </header>
         {component}
       </section>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './index.css';
