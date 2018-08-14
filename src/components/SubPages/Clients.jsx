5da4825535dc86ef1cfa38325822a56024abe72a
nathanolmstead <nathan@a2unedsolutions.com>
2018-08-14 15:46:09 -0400

    Clients page and logos

--- src/components/SubPages/Clients.jsx
+++ src/components/SubPages/Clients.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import lakewares from '../../images/lakewares.png';
+import fraza from '../../images/fraza.png';
+import metzgers from '../../images/metzgers.png';
+import hl7 from '../../images/hlz.gif';
 
 const Clients = () => {
   return (
     <div className="page-content">
       <p className="content-intro">We aren't even a year old yet and we already have some amazing clients.</p>
-      <ul className="grid-list">
-        <li>Lakewares</li>
-        <li>Disrupting Nate</li>
-        <li>Metzger's</li>
-        <li>Fraza</li>
-        <li>Artline</li>
-        <li>HL7</li>
+      <ul className="grid-list image-grid">
+        <li>
+          <a href="/">
+            <img src={lakewares} alt="Lakewares" />
+            <h2>Lake Apparel</h2>
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <img src={fraza} alt="Fraza Group" />
+            <h2>Forklifts &amp; Training</h2>
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <img src={metzgers} alt="Metzger's" />
+            <h2>Ann Arbor Restaurant</h2>
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <img src={hl7} alt="Health Level Seven" />
+            <h2>Health Level Seven</h2>
+          </a>
+        </li>
       </ul>
     </div>
   )
